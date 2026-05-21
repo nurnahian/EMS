03b4b96c4ff5ed0c7f65e5dbe08c66a3a011e9bd
nurnahian <nurnahian007@gmail.com>
2026-05-21 17:49:22 +0600

    feat:api call in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+export const backendUrl = "http://localhost:5001/api/employee";
 
 const App = () => {
-  return <div className="bg-amber-200">App</div>;
+  async function fetchEmployeeDetails() {
+    const result = await fetch(backendUrl);
+    const data = await result.json();
+    if (!result.ok) {
+      throw new Error(data.error);
+    }
+
+    return data;
+    // console.log(data);
+  }
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["employee_details"],
+    queryFn: fetchEmployeeDetails,
+  });
+
+  if (isPending) {
+    return <div>Loding...</div>;
+  }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
+  // useEffect(() => {
+  //   fetchEmployeeDetails();
+  // }, []);
+  return <div className="bg-amber-200 p-4">App</div>;
 };
 
 export default App;
